d7a1642498b42df4d1d63a81ae88e51ef1db3c3f
hanan ayman <hanan.ayman.said@gmail.com>
2021-07-29 00:05:49 +0200

    adding readme file

--- src/test/java/com/musala/task/controller/GatewayControllerTest.java
+++ src/test/java/com/musala/task/controller/GatewayControllerTest.java
@@ -7,13 +7,9 @@ import com.musala.task.service.GatewayService;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.hasSize;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.reset;
-import static org.mockito.Mockito.verify;
 import static org.hamcrest.Matchers.isA;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import java.math.BigDecimal;
@@ -23,7 +19,6 @@ import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.List;
 
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -34,10 +29,8 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 
 @RunWith(SpringRunner.class)
@@ -47,8 +40,6 @@ public class GatewayControllerTest {
     MockMvc mvc;
     @MockBean
     private GatewayService service;
-    @InjectMocks
-    GatewayController gatewayController;
     List<Gateway> gateways ;
     Gateway gateway;
     @Before
